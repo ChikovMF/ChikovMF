60a060dfaf66bcc03470c94affbbbdd0fe5c0a77
ChikovMF <ia-matvey@mail.ru>
2023-12-28 14:14:34 +0300

    Добавлены методы расширения для слоя Persistence и Application.

---
+++ ChikovMF.Application/ApplicationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ChikovMF.Application;
+
+public static class ApplicationExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
---
+++ ChikovMF.Persistence/PersistenceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ChikovMF.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChikovMF.Persistence;
+
+public static class PersistenceExtensions
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        string connectionString)
+    {
+        services.AddDbContext<ChikovMFDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<IChikovMFDbContext>(provider =>
+            provider.GetService<ChikovMFDbContext>() ?? 
+                throw new NullReferenceException(
+                    $"The {nameof(ChikovMFDbContext)} service is not connected to the project.")
+                );
+
+        return services;
+    }
+}
