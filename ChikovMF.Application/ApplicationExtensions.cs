d5a5b19d007f8a323948a2c73a7a0c34af1430d3
ChikovMF <ia-matvey@mail.ru>
2024-02-02 22:59:44 +0300

    Add AutoMapper in Application project

---
+++ ChikovMF.Application/ApplicationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ChikovMF.Application;
+
+public static class ApplicationExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
