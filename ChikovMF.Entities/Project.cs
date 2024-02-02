770e261ba1704fd1824266d5691d3899a23a6106
ChikovMF <ia-matvey@mail.ru>
2024-02-02 08:06:03 +0300

    Add entity Project

---
+++ ChikovMF.Entities/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace ChikovMF.Entities;
+
+public class Project
+{
+    public Guid ProjectId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Content { get; set; } = null!;
+}
