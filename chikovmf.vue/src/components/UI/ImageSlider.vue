4b4450c37ae6da84fc76168e9715e5aabdf2b403
ChikovMF <ia-matvey@mail.ru>
2024-02-17 05:04:01 +0300

    Add ui component ImageSlider in Vue project

---
+++ chikovmf.vue/src/components/UI/ImageSlider.vue
@@ -0,0 +1,72 @@
+<template>
+    <div v-if="images">
+
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item" v-for="image in images" :class="{ active: this.isActive(image.projectImageId) }">
+                    <img :src="image.src" :alt="image.alt" class="d-block w-100">
+                </div>
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev"
+                v-on:click="onPrev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next"
+                v-on:click="onNext">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ImageSlider",
+    data() {
+        return {
+            activeSlideIndex: 0,
+            slideCount: 0
+        }
+    },
+    props: {
+        images: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        onNext() {
+            if (this.activeSlideIndex >= this.slideCount - 1) {
+                this.activeSlideIndex = 0;
+            }
+            else {
+                this.activeSlideIndex++;
+            }
+        },
+        onPrev() {
+            if (this.activeSlideIndex <= 0) {
+                this.activeSlideIndex = this.slideCount - 1;
+            }
+            else {
+                this.activeSlideIndex--;
+            }
+        },
+        isActive(imageId) {
+            if (imageId == this.images[this.activeSlideIndex].projectImageId) {
+                return true;
+            }
+            return false;
+        }
+    },
+    created() {
+        this.slideCount = Object.keys(this.images).length;
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- chikovmf.vue/src/components/UI/index.js
+++ chikovmf.vue/src/components/UI/index.js
@@ -2,10 +2,12 @@ import PageTitle from "@/components/UI/PageTitle.vue";
 import SuccessAlert from "@/components/UI/SuccessAlert.vue";
 import ErrorAlert from "@/components/UI/ErrorAlert.vue";
 import Spinner from "@/components/UI/Spinner.vue";
+import ImageSlider from "@/components/UI/ImageSlider.vue";
 
 export default [
     PageTitle,
     SuccessAlert,
     ErrorAlert,
-    Spinner
+    Spinner,
+    ImageSlider
 ];
