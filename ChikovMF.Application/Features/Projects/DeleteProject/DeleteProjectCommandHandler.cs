92a07e3a112392c012912f13e33012c94225db2c
ChikovMF <ia-matvey@mail.ru>
2024-02-18 20:53:05 +0300

    Update DeleteProject (delete projectImage file)

--- ChikovMF.Application/Features/Projects/DeleteProject/DeleteProjectCommandHandler.cs
+++ ChikovMF.Application/Features/Projects/DeleteProject/DeleteProjectCommandHandler.cs
@@ -4,29 +4,40 @@ using ChikovMF.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace ChikovMF.Application.Features.Projects.DeleteProject
+namespace ChikovMF.Application.Features.Projects.DeleteProject;
+
+public class DeleteProjectCommandHandler : IRequestHandler<DeleteProjectCommand>
 {
-    public class DeleteProjectCommandHandler : IRequestHandler<DeleteProjectCommand>
+    public async Task Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
     {
-        public async Task Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
-        {
-            var project = await _context.Projects
-                .FirstOrDefaultAsync(p => p.ProjectId == request.ProjectId, cancellationToken);
+        var project = await _context.Projects
+            .Include(p => p.Images)
+            .FirstOrDefaultAsync(p => p.ProjectId == request.ProjectId, cancellationToken);
 
-            if (project == null)
+        if (project == null)
+        {
+            throw new NotFoundEntityException(nameof(Project), request.ProjectId);
+        }
+
+        string pathLocation = Path.Combine(Directory.GetCurrentDirectory(), $"Images");
+
+        foreach (var image in project.Images!)
+        {
+            string saveLocation = Path.Combine(pathLocation, image.FileName);
+            if (File.Exists(saveLocation) && !string.IsNullOrWhiteSpace(image.FileName))
             {
-                throw new NotFoundEntityException(nameof(Project), request.ProjectId);
+                File.Delete(saveLocation);
             }
-
-            _context.Projects.Remove(project);
-            await _context.SaveChangesAsync(cancellationToken);
         }
 
-        private readonly IChikovMFContext _context;
+        _context.Projects.Remove(project);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
 
-        public DeleteProjectCommandHandler(IChikovMFContext context)
-        {
-            _context = context;
-        }
+    private readonly IChikovMFContext _context;
+
+    public DeleteProjectCommandHandler(IChikovMFContext context)
+    {
+        _context = context;
     }
 }
