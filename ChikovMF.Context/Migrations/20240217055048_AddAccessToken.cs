8197fc7395bd89e0cab6d6d5c0f206823b6c5baa
ChikovMF <ia-matvey@mail.ru>
2024-02-18 18:07:41 +0300

    Add DbSet<AccessToken> in context and add migrations

---
+++ ChikovMF.Context/Migrations/20240217055048_AddAccessToken.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChikovMF.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccessToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccessTokens",
+                columns: table => new
+                {
+                    AccessTokenId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DateOfReceiving = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HashToken = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessTokens", x => x.AccessTokenId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccessTokens");
+        }
+    }
+}
