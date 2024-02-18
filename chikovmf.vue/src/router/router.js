b3e16d904798a51aea67075f29b8a2f385374db1
ChikovMF <ia-matvey@mail.ru>
2024-02-18 18:16:40 +0300

    Update router (add pages and meta:requireAuth)

--- chikovmf.vue/src/router/router.js
+++ chikovmf.vue/src/router/router.js
@@ -8,40 +8,89 @@ import ListProjectPage from '@/pages/projects/ListProjectPage.vue';
 import ListTagPage from '@/pages/tags/ListTagPage.vue';
 import CreateTagPage from '@/pages/tags/CreateTagPage.vue';
 import EditTagPage from '@/pages/tags/EditTagPage.vue';
+import LoginPage from '@/pages/LoginPage.vue';
+import AccessDeniedPage from '@/pages/AccessDeniedPage.vue'
+import NotFoundPage from '@/pages/NotFoundPage.vue'
 
 const routes = [
     {
         path: '/',
-        component: MainPage
+        component: MainPage,
+        meta: {
+            requireAuth: false,
+        }
     },
     {
         path: '/Projects/Create',
-        component: CreateProjectPage
+        component: CreateProjectPage,
+        meta: {
+            requireAuth: true,
+        }
     },
     {
         path: '/Projects',
-        component: ListProjectPage
+        component: ListProjectPage,
+        meta: {
+            requireAuth: false,
+        }
     },
     {
         path: '/Projects/Edit/:projectId',
-        component: EditProjectPage
+        component: EditProjectPage,
+        meta: {
+            requireAuth: true,
+        }
     },
     {
         path: '/Projects/:projectId',
-        component: DetailProjectPage
+        component: DetailProjectPage,
+        meta: {
+            requireAuth: false,
+        }
     },
     {
         path: '/Tags',
-        component: ListTagPage
+        component: ListTagPage,
+        meta: {
+            requireAuth: true,
+        }
     },
     {
         path: '/Tags/Create',
-        component: CreateTagPage
+        component: CreateTagPage,
+        meta: {
+            requireAuth: true,
+        }
     },
     {
         path: '/Tags/Edit/:tagId',
-        component: EditTagPage
+        component: EditTagPage,
+        meta: {
+            requireAuth: true,
+        }
     },
+    {
+        path: '/Login',
+        component: LoginPage,
+        meta: {
+            requireAuth: false,
+        }
+    },
+    {
+        path: '/AccessDenied',
+        component: AccessDeniedPage,
+        meta: {
+            requireAuth: false,
+        }
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: '404',
+        component: NotFoundPage,
+        meta: {
+            requireAuth: false,
+        }
+    }
 ]
 
 const router = createRouter({
