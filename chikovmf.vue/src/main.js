a1316fd46bfaffc146362044996f162e67e972ac
ChikovMF <ia-matvey@mail.ru>
2024-02-18 18:17:40 +0300

    Update main.js (add filter available pages)

--- chikovmf.vue/src/main.js
+++ chikovmf.vue/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import components from '@/components/UI';
 import router from '@/router/router';
+import store from '@/store'
 
 const app = createApp(App);
 
@@ -9,5 +10,16 @@ components.forEach(component => {
     app.component(component.name, component);
 });
 
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requireAuth) && !store.state.isAuth) {
+        next({
+            path: '/AccessDenied',
+        });
+    } else {
+        next();
+    }
+});
+
 app.use(router)
+    .use(store)
     .mount('#app')
