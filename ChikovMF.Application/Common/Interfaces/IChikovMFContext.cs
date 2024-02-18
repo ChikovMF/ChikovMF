9113b0825ba13157a18ca95d338794a3224626b9
ChikovMF <ia-matvey@mail.ru>
2024-02-18 18:06:49 +0300

    Add AccessToken entity

--- ChikovMF.Application/Common/Interfaces/IChikovMFContext.cs
+++ ChikovMF.Application/Common/Interfaces/IChikovMFContext.cs
@@ -1,12 +1,13 @@
 ﻿using ChikovMF.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace ChikovMF.Application.Interfaces;
+namespace ChikovMF.Application.Common.Interfaces;
 
 public interface IChikovMFContext
 {
     DbSet<Project> Projects { get; set; }
     DbSet<Tag> Tags { get; set; }
+    DbSet<AccessToken> AccessTokens { get; set; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
---
+++ ChikovMF.Entities/AccessToken.cs
@@ -0,0 +1,8 @@
+﻿namespace ChikovMF.Entities;
+
+public class AccessToken
+{
+    public Guid AccessTokenId { get; set; }
+    public DateTime DateOfReceiving { get; set; }
+    public string HashToken { get; set; } = null!;
+}
