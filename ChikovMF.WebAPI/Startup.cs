0d58b5c9efe24736cabac1124bfe955aeee0056f
ChikovMF <ia-matvey@mail.ru>
2024-02-18 18:11:14 +0300

    Add JwtBearer in ASP Api

--- ChikovMF.WebAPI/Startup.cs
+++ ChikovMF.WebAPI/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using ChikovMF.Application;
+using ChikovMF.Application.Common.Interfaces;
 using ChikovMF.Application.Common.Mappings;
-using ChikovMF.Application.Interfaces;
 using ChikovMF.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
+using System.Text;
 
 namespace ChikovMF.WebAPI;
 
@@ -11,6 +15,21 @@ public static class Startup
 {
     public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JwtAuth:Issuer"],
+                    ValidAudience = configuration["JwtAuth:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtAuth:Key"] ?? throw new ArgumentNullException("JwtAuth:Key string not found.")))
+                };
+            });
+
         services.AddControllers();
 
         services.AddAutoMapper(config =>
@@ -23,7 +42,32 @@ public static class Startup
         services.AddApplication(configuration);
 
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(option =>
+        {
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+ {
+     {
+           new OpenApiSecurityScheme
+             {
+                 Reference = new OpenApiReference
+                 {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "Bearer"
+                 }
+             },
+             new string[] {}
+     }
+ });
+        });
     }
 
     public static void Configure(this IApplicationBuilder app, IWebHostEnvironment env)
@@ -49,6 +93,7 @@ public static class Startup
             app.UseCors();
         }
 
+        app.UseAuthentication();
         app.UseAuthorization();
     }
 }
