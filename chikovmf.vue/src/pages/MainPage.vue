8221131610ecc673d44d0d3de49f74b203871dcf
Chikov Matvey Fedorovich <ia-matvey@mail.ru>
2024-02-21 05:32:00 +0300

    Update MainPage in vue project

--- chikovmf.vue/src/pages/MainPage.vue
+++ chikovmf.vue/src/pages/MainPage.vue
@@ -1,16 +1,54 @@
 <template>
     <PageTitle>
-        Главная страница
+        Обо мне
     </PageTitle>
 
-    <div>
-        <h1>Главная страница!</h1>
-        <router-link to="/Projects">Проекты</router-link>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-sm-3 d-sm-block d-none">
+                <img src="@/img/me.jpg" class="img-fluid border" alt="Мое фото">
+            </div>
+            <div class="col-sm-9">
+                <h1>Здравствуйте, я Матвей</h1>
+                <p>Я начинающий разработчик. Мне нравится создавать различные приложения. Этот сайт - мое веб-портфолио. На
+                    нем содержится основная информация обо мне как о разработчике, а также некоторые проекты которые я
+                    создал.</p>
+            </div>
+        </div>
     </div>
 
-    <p>
-        {{ $store.state.isAuth }}
-    </p>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Ключевые навыки:</h2>
+                <ul class="list-group list-group-horizontal-sm">
+                    <li class="list-group-item">C#</li>
+                    <li class="list-group-item">ASP.NET</li>
+                    <li class="list-group-item">Entity Framework</li>
+                    <li class="list-group-item">xUnit</li>
+                    <li class="list-group-item">JavaScript</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Мои контакты:</h2>
+                <ul>
+                    <li>
+                        Email:
+                        <a href="mailto:ia-matvey@mail.ru">ia-matvey@mail.ru</a>
+                    </li>
+                    <li>
+                        Telegram:
+                        <a href="tg://resolve?domain=ChikovMF">ChikovMF</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
