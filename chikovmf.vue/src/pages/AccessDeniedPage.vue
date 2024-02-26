d9f70b0fd3ad60d29b023a4f5bc89ec148951bb4
Chikov Matvey Fedorovich <ia-matvey@mail.ru>
2024-02-26 23:22:56 +0300

    Update AccessDeniedPage

--- chikovmf.vue/src/pages/AccessDeniedPage.vue
+++ chikovmf.vue/src/pages/AccessDeniedPage.vue
@@ -1,12 +1,23 @@
 <template>
-    <div>
-        AccessDenied
+    <PageTitle>
+        Доступ к запрошенному ресурсу запрещен (Access Denied 403)
+    </PageTitle>
+
+    <div class="container w-50 text-center">
+        <img src="@/img/accessDenied.jpg" class="img-fluid" alt="Доступ запрещен">
+        <br />
+        <button class="btn" v-on:click="routeMain">На главную</button>
+        <button class="btn" v-on:click="this.$router.push('/Login')">Войти</button>
     </div>
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            routeMain() {
+                this.$router.push("/");
+            }
+        }
     }
 </script>
 
