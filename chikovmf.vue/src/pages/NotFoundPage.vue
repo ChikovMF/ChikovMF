ef5dcdb37c734b94e7969fe72de0610c31cf0cf0
Chikov Matvey Fedorovich <ia-matvey@mail.ru>
2024-02-26 23:22:36 +0300

    Update NotFoundPage

--- chikovmf.vue/src/pages/NotFoundPage.vue
+++ chikovmf.vue/src/pages/NotFoundPage.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        NotFound
+    <PageTitle>
+        Страница не найдена (Not Found 404)
+    </PageTitle>
+
+    <div class="container w-50 text-center">
+        <img src="@/img/notFound.jpg" class="img-fluid" alt="Страница не найдена">
+        <br />
+        <button class="btn" v-on:click="routeMain">На главную</button>
     </div>
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            routeMain() {
+                this.$router.push("/");
+            }
+        }
     }
 </script>
 
